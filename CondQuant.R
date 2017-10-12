097e0efb2556243c8fa99d30535da622c4de2458
Matt Dawson <mwdawson@ucdavis.edu>
2017-10-11 22:43:49 -0700

    added a helper function and a readme

--- CondQuant.R
+++ CondQuant.R
@@ -1,6 +1,6 @@
-##################################################
-### Auxiliary functions ##########################
-##################################################
+################################################################################
+### Auxiliary functions ########################################################
+################################################################################
 
 # Gaussian kernel function
 # x0: the value at which to estimate the function
@@ -10,12 +10,10 @@ K <- function(x0, x, b) {
   exp(-.5 * ((x - x0)/b)^2)
 }
 
-# Estimation of a smooth conditional cdf F(z|x)
-# using a Gaussian kernel
-# z0: the value at which to estimate the
-#     conditional cdf
-# x0: the conditioning value at which to estimate
-#     the conditonal cdf
+# Estimation of a smooth conditional cdf F(z|x) at (z0, x0) using a Gaussian 
+# kernel
+# z0: the value at which to estimate the conditional cdf
+# x0: the conditioning value at which to estimate the conditonal cdf
 # z: a vector of points neighboring z0
 # x: a vector of points neighboring x0
 # b.z: the bandwidth in the z direction
@@ -25,3 +23,23 @@ SmoothCondCdf <- function(z0, x0, z, x, b.z, b.x) {
     sum(K(x0=x0, x=x, b=b.x))
 }
 
+# Estimation of a conditional quantile of Z|X = x0 using a Gaussian kernel
+# alpha: a vector of probabilities
+# x0: the conditioning value at which to estimate the quantile
+# zGrid: the grid to be used for estimating the quantile
+# z: a vector of points neighboring z0
+# x: a vector of points neighboring x0
+# b.z: the bandwidth in the z direction
+# b.x: the bandwidth in the x direction
+CondQuant <- function(alpha, x0, zGrid, z, x, b.z, b.x) {
+  
+  # estimate the conditional cdf
+  cdf <- sapply(zGrid, function(y) {
+    SmoothCondCdf(z0=y, x0=x0, z=z, x=x, b.z=b.z, b.x=b.x)
+  })
+  
+  # extract the quantile
+  quant <- sapply(alpha, function(a) zGrid[which.min(abs(cdf - a))])
+  
+  return(quant)
+}
